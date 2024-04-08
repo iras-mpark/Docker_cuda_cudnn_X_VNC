83cf0584e123289f6134fcb3bb2a2bd8c1e70715
iras-mpark <mpark@changwon.ac.kr>
2024-04-09 02:20:17 +0900

    upload

---
+++ ROS2_X/run.sh
@@ -0,0 +1,11 @@
+docker run -it \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="/dev:/dev" \
+    --device=/dev/nvidiactl \
+    --device=/dev/nvidia-uvm \
+    --device=/dev/nvidia0 \
+    --privileged \
+    --name mk_ros2_x \
+    mk:ros2_x
